4aec5c24075b13ecf328f76905a899bd22050c9c
guomiao01 <guomiao01@baidu.com>
2016-06-14 23:41:04 +0800

    add double click event

--- js/initital.js
+++ js/initital.js
@@ -99,11 +99,97 @@ require(['jquery', 'peaks'], function ($, Peaks) {
             $('#text_' + segment.id + ' .start-time').html(globalSegments[textId].startTime);
             $('#text_' + segment.id + ' .js_alltime').html(globalSegments[textId].alltime + 'seconds');
             $('#text_' + segment.id + ' .js_wps').html(wps + 'WPS');
+        },
+
+        /**
+         * 添加textarea
+         *
+         * @param {Object} peaksInstance instance实例
+         */
+        addTextArea: function (peaksInstance) {
+          var segmentObj = {};
+            // A new segment created
+          var allSegments = peaksInstance.segments.getSegments();
+          var currentSegment = allSegments[allSegments.length -1];
+          var startTime = currentSegment.startTime;
+          var endTime = currentSegment.endTime;
+          var textId = 'text_' + (currentSegment.id || 'segment');
+          segmentObj.textId = textId;
+          segmentObj.startTime = utility.getTime(startTime);
+          segmentObj.alltime = Math.round((endTime - startTime) * 100) / 100;
+          segmentObj.wordsCount = 0;
+          // edit words
+          // add a li
+          var li = [
+              '<li class="active" data-pindex="29" data-index="29" id="{@textId}">',
+                    '<div class="subtitle">',
+                        '<div class="subspan">',
+                              '<span class="start-time">{@startTime}</span>',
+                        '</div>',
+                        '<div class="sub-content">',
+                             '<div class="txt">Koreanclass101.com Hanna Hanna Hangul</div>',
+                             '<textarea name="" cols="30" rows="10" placeholder="edit..."></textarea>',
+                        '</div>',
+                        '<div class="subspan">',
+                            '<span class="js_wps">{@wps}</span>',
+                            '<span class="js_alltime">{@alltime}seconds</span>',
+                            '<span class="js_wordsCount">0word</span>',
+                            '<span class="delete-icon">——</span>',
+                        '</div>',
+                    '</div>',
+                    '<div class="buttomline"></div>',
+                 '</li>'
+          ].join('');
+          li = li.replace('{@startTime}', segmentObj.startTime)
+                 .replace('{@alltime}', segmentObj.alltime)
+                 .replace('{@textId}', segmentObj.textId)
+                 .replace('{@wps}', '0WPS');
+          $('#contentList').prepend(li);  
+          // store in global parametes
+          globalSegments[textId] = segmentObj;      
+          // tetxarea监听以修改字数等信息
+          $('#' + textId + ' textarea').on('propertychange input', function () {
+              // TODO more accurate
+              var wordsCount = $(this).val().replace(/\s+$/, '').split(/\s+/).length;
+              globalSegments[textId].wordsCount = wordsCount;
+              var alltime = globalSegments[textId].alltime;
+              var wps = alltime ? Math.round(wordsCount / alltime * 100) / 100 : 0;
+              $(this).parent().parent().find('.js_wordsCount').html(wordsCount + 'words');
+              $(this).parent().parent().find('.js_wps').html(wps + 'WPS');
+          }).blur(function () {
+              // store the content in localstorage
+              localStorage.setItem('textId', $(this).val());
+              // when send?
+              // editable?
+          })
+        },
+
+        /**
+         * 封装后的添加片段事件
+         *
+         * @param {Object} instance instance实例
+         * @param {Object} segment 要添加的片段
+         */
+        addSegment: function (instance, segment) {
+          // 第一步添加片段
+          instance.segments.add([segment]);
+          // 第二步添加textarea
+          this.addTextArea(instance);
         }
     }
     peaksInstance.on('segments.dragged', function (segment) {
+        console.log(segment);
         // change content TODO容错处理
         utility.updateData(segment);        
+    })
+    peaksInstance.on('dbclickAddSegment', function () {
+        console.log('dbclickAddSegment');
+        var segment = {
+          startTime: peaksInstance.time.getCurrentTime(),
+          endTime: peaksInstance.time.getCurrentTime() + 1,
+          editable: true
+        }
+        utility.addSegment(peaksInstance, segment);
     })
     $('#createSegment').click(function (e) {
         var segmentObj = {};
